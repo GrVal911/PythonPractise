6aaf8983e2c742dac57d777129bab6622955909c
GrVal911 <grvalya911@mail.ru>
2019-07-12 13:13:13 +0300

    Nielsen (update)

--- Other/Nielsen/nielsen.py
+++ Other/Nielsen/nielsen.py
@@ -2,37 +2,75 @@ import numpy as np
 import pandas as pd
 
 data=pd.read_csv('milk_promo_sales.csv')
-unq=np.unique(data['store_id']) #ID всех магазинов
-allDuration=np.array(np.empty(0,dtype=int))
-allCount=np.array(np.empty(0,dtype=int))
-sum=np.array(np.empty(0,dtype=int))
-for unique in unq: #Просмотр промопериодов по каждому магазину
-    data = pd.read_csv('milk_promo_sales.csv')
-    data=data.loc[data['store_id']==unique] # Оставляем данные только о текущем магазине
-    data=data.sort_values(by=['period_id']) #Сортируем недели по порядку
-    # print(data)
-    count=1
-    duration=1
-    strt=0
-    for i in range(len(data)-1):
-        if data.iloc[i+1,1]-data.iloc[i,1]>1: # Если промопериод закончился
-            count+=1
-            allDuration=np.append(allDuration,duration) #Записываем длительность текущего промопериода
-            duration=1
-            sum=np.append(sum,np.sum(data.iloc[strt:i+1,2])) #Записываем объем продаж в течение данного промопериода
-            strt = i + 1 # Запоминаем начало следующего промопериода
-        else:
-            duration+=1
-    allDuration=np.append(allDuration,duration) #Записываем данные о последнем промопериоде для данного магазина
-    sum=np.append(sum,np.sum(data.iloc[strt:len(data),2]))
-    allCount=np.append(allCount,count)
-    # print(allCount)
-    # print(allDuration)
-    # print(sum)
+# unq=np.unique(data['store_id']) #ID всех магазинов
+allDuration=np.array([1])
+allCount=0
+sum=0
+tempSum=0
+data=data.sort_values(by=['store_id','period_id'])
+# print(data)
+prev_id=0
+prev_per=0
+count=1
+duration=1
+def func(row):
+    global  allDuration, allCount, sum, tempSum, prev_id, prev_per, count, duration
+    if row['period_id']-prev_per>1 and row['store_id']==prev_id:
+        prev_per=row['period_id']
+        count += 1
+        allCount=count
+        allDuration =np.append(allDuration,1)
+        duration = 1
+        if tempSum==0:
+            sum=row['sales_volume']
+        tempSum=0
+    elif row['store_id']!=prev_id:
+        prev_id = row['store_id']
+        prev_per = row['period_id']
+        allDuration =np.array([1])
+        sum=row['sales_volume']
+        # sum=0
+        tempSum = 0
+        allCount=count
+        count = 1
+        duration = 1
+    elif row['period_id']-prev_per==1 and row['store_id']==prev_id:
+        prev_per = row['period_id']
+        duration+=1
+        allDuration[count-1]=duration
+        tempSum += row['sales_volume']
+        sum+=tempSum
+        allCount=count
+    return np.array2string(allDuration)+'_'+str(allCount)+'_'+str(round(sum,2))
 
-print(np.sum(allCount)) #1. Общее количество промопериодов (во всех магазинах)
-print(np.median(allDuration)) #2. Медиана продолжительности промопериода (количество недель)
-print(sum) #3. Объем  продаж по каждому промопериоду
-print(np.median(allCount)) #4. Медиана количества промопериодов на один магазин
+data['allDuration']=data.apply(lambda row: func(row), axis=1)
+
+def splt1(row):
+    return row.split('_')[0].replace('[','').replace(']','')+' '
+def splt2(row):
+    return int(row.split('_')[1])
+def splt3(row):
+    return float(row.split('_')[2])
+
+data['allCount']=data['allDuration'].apply(lambda row: splt2(row))
+data['sum']=data['allDuration'].apply(lambda row: splt3(row))
+data['allDuration']=data['allDuration'].apply(lambda row: splt1(row))
+
+data=data.drop_duplicates(subset='store_id',keep='last')
+
+print(np.sum(data['allCount'])) #1. Общее количество промопериодов (во всех магазинах)
+print(np.median(np.fromstring(np.sum(data['allDuration']),dtype=int,sep=' '))) #2. Медиана продолжительности промопериода (количество недель)
+print(data['sum']) #3. Объем  продаж по каждому промопериоду
+print(np.median(data['allCount'])) #4. Медиана количества промопериодов на один магазин
+
+# data.to_csv('ansCSV.csv',index=False)
+# data.to_excel('ansEx.xls',index=False)
+
+# 1. 141158
+# 2. 3
+# 3. Ответ в ansCSV.csv в колонке 'sum'
+# 4. 6
+
+
 
 
